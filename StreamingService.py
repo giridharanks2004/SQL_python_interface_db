453cd6602d709084d271a3c347f5e151e40e443f
giridharanks2004 <iamgiridharanks@gmail.com>
2022-08-29 10:20:43 +0530

    changed to link

--- StreamingService.py
+++ StreamingService.py
@@ -12,7 +12,7 @@ while cur.is_connected:
     break
 choicenter='y'
 while choicenter == 'y':
-    print("----------WELCOME TO HBOMAX STREAMDATABASE LINK by harishiv-----------")
+    print("----------WELCOME TO HBOMAX STREAMDATABASE LINK-----------")
     print("**********a WARNERMEDIA company************")
     print("1.ORIGINAL PRODUCIONS")
     print("2.LICENSED CONTENT")
