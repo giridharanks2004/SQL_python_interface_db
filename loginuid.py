ea0a3ba1f17c8cfae462ba14c47b609f12333d16
giridharanks2004 <112338245+giridharanks2004@users.noreply.github.com>
2022-08-30 16:20:21 +0530

    finished code

--- loginuid.py
+++ loginuid.py
@@ -4,9 +4,9 @@ import mysql.connector as m
 curs=m.connect(host="localhost",user='root',passwd="3000")
 cursor=curs.cursor()
 while curs.is_connected:
-    cursor.execute("create database vodpass")
+    #cursor.execute("create database vodpass")
     cursor.execute("use vodpass;")
-    cursor.execute("create table userinfo(uid varchar(10),paswd varchar(10));")
+    #cursor.execute("create table userinfo(uid varchar(10),paswd varchar(10));")
     curs.commit()
     break
 cho="y"
@@ -15,7 +15,7 @@ while cho=='y':
     print("1.LOGIN")
     print("2.CREATE USER ID")
     ch1=int(input("enter your choice to proceed:"))
-    while ch1==1:
+    if ch1==1:
         inp1=input("enter you user id:")
         cursor.execute('select * from userinfo;')
         check=cursor.fetchall()
@@ -29,4 +29,19 @@ while cho=='y':
                     print('enter correct pwd!')
             else:
                 print('user info not found')
+    elif ch1==2:
+        inpun=input('enter valid user name:')
+        inpwd=input("enter valid password:")
+        if isinstance(inpun,str):
+            cursor.execute("insert into userinfo values('{}','{}');".format(inpun,inpwd))
+            curs.commit()
+            print('the user info has been added sign in to continue')
+        else:
+            print("enter proper uid and password to sign up!")
+    cho=input('do you want sign in again? y/n:')
+    
+            
+        
+        
+                
             
